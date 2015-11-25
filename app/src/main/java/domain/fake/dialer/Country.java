d37d3637b50d8618bf2dbfc243282c5d1c325d70
Matthijs Reukers <thundertactics@gmail.com>
2015-11-25 17:06:19 +0100

    Done.
    -Checks whether phone number is valid
    -Shows Wikipedia page of country
    -Made a translation (NL)

--- app/src/main/java/domain/fake/dialer/Country.java
+++ app/src/main/java/domain/fake/dialer/Country.java
@@ -4,12 +4,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
 
 public class Country extends AppCompatActivity {
+    private WebView myWebView;
 
 
     @Override
@@ -26,11 +32,32 @@ public class Country extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri number = Uri.parse("tel:"+message);
+                Uri number = Uri.parse("tel:" + message);
                 Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
                 startActivity(callIntent);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        String country = getCountry(message);
+        myWebView.loadUrl("http://en.wikipedia.org/wiki/"+country);
+    }
+
+    public String getCountry(String phoneNumber)
+    {
+        PhoneNumberUtil pUtil = PhoneNumberUtil.getInstance();
+        Phonenumber.PhoneNumber pNum = null;
+        try {
+            pNum = pUtil.parse(phoneNumber, "ZZ");
+
+        } catch (NumberParseException e) {
+            System.err.println("NumberParseException was thrown:" + e.toString());
+        }
+
+        String regionCode = pUtil.getRegionCodeForNumber(pNum);
+        String country = "ISO_3166-1:" + regionCode;
+        return country;
     }
 }
--- app/src/main/java/domain/fake/dialer/MainActivity.java
+++ app/src/main/java/domain/fake/dialer/MainActivity.java
@@ -1,11 +1,17 @@
 package domain.fake.dialer;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+
 import java.lang.String;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private String pNumber = "";
+    private String pNumber = "+";
 
     public void onButtonClick(View v) {
         Button button = (Button) v;
@@ -34,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     public void backButtonClick(View v)
     {
         Button dialButton = (Button) findViewById(R.id.button);
-        if(pNumber.length() > 0)
+        if(pNumber.length() > 1)
         {
             pNumber = pNumber.substring(0, pNumber.length() - 1);
         }
@@ -46,8 +52,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void dialButtonClick(View view) {
-        //check country first
-        sendMessage(view);
+        PhoneNumberUtil pUtil = PhoneNumberUtil.getInstance();
+        boolean validNumber = true;
+        try {
+            pUtil.parse(pNumber, "ZZ");
+
+        } catch (NumberParseException e) {
+            validNumber = false;
+        }
+        if(validNumber)
+            sendMessage(view);
+        else
+            errorPopup(view);
+    }
+
+    public void errorPopup(View view)
+    {
+        Snackbar mySnackbar = Snackbar.make(view, R.string.invalid_number, Snackbar.LENGTH_SHORT);
+        mySnackbar.show();
     }
 
     public void sendMessage(View view)
